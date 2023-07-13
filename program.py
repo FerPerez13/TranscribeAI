68732e079d383a7ae1bb1196de2096867cb268e5
Fernando Perez <fperez@bavieracb.onmicrosoft.com>
2023-07-13 11:39:55 +0200

    Fix problem with large files

--- program.py
+++ program.py
@@ -2,11 +2,11 @@
 from tkinter import Tk, Label, Button, filedialog
 import speech_recognition
 
+
 class TranscribeAI:
     filePath = ""
 
     def __init__(self, master):
-
         master.geometry("400x300")
         master.resizable(False, False)
 
@@ -16,17 +16,26 @@ class TranscribeAI:
         self.label = Label(master, text="Bienvenia a Transcribe AI")
         self.label.pack()
 
-        self.label = Label(master, text="Selecciona un archivo en formato .wav \n y presiona el botón 'Enviar a Azure'")
+        self.label = Label(
+            master,
+            text="Selecciona un archivo en formato .wav \n y presiona el botón 'Enviar a Azure'",
+        )
         self.label.pack()
 
         # Boton para seleccionar un archivo de audio
-        self.openDocumentButton = Button(master, text="Seleccionar archivo", command=self.select_file)
+        self.openDocumentButton = Button(
+            master, text="Seleccionar archivo", command=self.select_file
+        )
         self.openDocumentButton.pack()
 
-        self.documentSelected = Label(master, text=f"Archivo seleccionado: {TranscribeAI.filePath}")
+        self.documentSelected = Label(
+            master, text=f"Archivo seleccionado: {TranscribeAI.filePath}"
+        )
         self.documentSelected.pack()
 
-        self.sendToAzureButton = Button(master, text="Enviar a Azure", state="disabled", command=self.send_to_azure)
+        self.sendToAzureButton = Button(
+            master, text="Enviar a Azure", state="disabled", command=self.send_to_azure
+        )
         self.sendToAzureButton.pack()
 
         self.messages = Label(master, text="Esperando archivo...")
@@ -38,26 +47,31 @@ class TranscribeAI:
         file = open(file_name, "w")
         file.write(text)
         file.close()
-        
+
         self.messages.configure(text=f"Completado!!!!")
-        
+
         TranscribeAI.filePath = ""
         self.sendToAzureButton.configure(state="disabled")
-        self.documentSelected.configure(text=f"Archivo seleccionado: {TranscribeAI.filePath}")
+        self.documentSelected.configure(
+            text=f"Archivo seleccionado: {TranscribeAI.filePath}"
+        )
 
-    def send_to_azure(self):        
+    def send_to_azure(self):
         self.messages.configure(text=f"Enviando y transcribiendo ...")
-        result = speech_recognition.from_file( TranscribeAI.filePath)
-        TranscribeAI.create_document_with_text(self ,result)
+        result = speech_recognition.from_file(TranscribeAI.filePath)
+        TranscribeAI.create_document_with_text(self, result)
 
     def select_file(self):
         TranscribeAI.filePath = filedialog.askopenfilename()
-        
-        self.documentSelected.configure(text=f"Archivo seleccionado: \n {TranscribeAI.filePath}")
+
+        self.documentSelected.configure(
+            text=f"Archivo seleccionado: \n {TranscribeAI.filePath}"
+        )
         self.messages.configure(text=f"Listo para Enviar a Azure")
 
         self.sendToAzureButton.configure(state="normal")
-            
+
+
 root = Tk()
 miVentana = TranscribeAI(root)
 root.mainloop()
--- speech_recognition.py
+++ speech_recognition.py
@@ -1,10 +1,43 @@
-import azure.cognitiveservices.speech as speech 
+import azure.cognitiveservices.speech as speech
+import time
+from tkinter import HORIZONTAL, VERTICAL, Frame, Scrollbar, Tk, Label, Text
+
 
 def from_file(filepath):
-    speech_config = speech.SpeechConfig(subscription="8778e83e29594daa8666cf7d147d3c42", region="eastus", speech_recognition_language="es-ES")
+    # Llamada a Azure
+    speech_config = speech.SpeechConfig(
+        subscription="8778e83e29594daa8666cf7d147d3c42",
+        region="eastus",
+        speech_recognition_language="es-ES",
+    )
     audio_config = speech.audio.AudioConfig(filename=filepath)
-    speech_recognizer = speech.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    speech_recognizer = speech.SpeechRecognizer(
+        speech_config=speech_config, audio_config=audio_config
+    )
 
-    result = speech_recognizer.recognize_once()
-    #return result.json
-    return result.text
+    done = False
+    textOut = ""
+
+    def stop_cb(evt):
+        print("CLOSING on {}".format(evt))
+        speech_recognizer.stop_continuous_recognition()
+        nonlocal done
+        done = True
+
+    str_newLine = " \n"
+
+    def outPrint(evt):
+        nonlocal textOut
+        tmp_text = evt.result.text
+        textOut += tmp_text + str_newLine
+        print(tmp_text)
+
+    speech_recognizer.recognized.connect(outPrint)
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.start_continuous_recognition()
+
+    while not done:
+        time.sleep(0.5)
+
+    return textOut
+    # return result.text
